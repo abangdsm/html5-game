58e6237c610ffcf03b788f34e39eddd0a005b5f8
abangdsm <id.reversal@gmail.com>
2023-01-23 02:47:51 +0700

    Implementasi trigonometri untuk efek gelombang pada enemy

--- enemy-types/script.js
+++ enemy-types/script.js
@@ -19,20 +19,25 @@ let gameFrame = 0;
 class Enemy {
     constructor() {
         this.image = new Image();
-        this.image.src = 'enemy1.png';
-        // this.speed = Math.random() * 4 - 2;
-        this.sprWidth = 293;
-        this.sprHeight = 155;
+        this.image.src = 'enemy2.png';
+        this.speed = Math.random() * 4 + 1;
+        this.sprWidth = 266;
+        this.sprHeight = 188;
         this.width = this.sprWidth / 2.5;
         this.height = this.sprHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+        this.angle = 0;
+        this.angleSpeed = Math.random() * 0.2;
+        this.curve = Math.random() * 10;
     }
     update(){
-        this.x += Math.random() * 5 - 2.5; 
-        this.y += Math.random() * 5 - 2.5;
+        this.x -= this.speed; 
+        this.y += this.curve * Math.sin(this.angle);
+        this.angle += this.angleSpeed;
+        if (this.x + this.width < 0) this.x = canvas.width;
         // animate sprites
         if(gameFrame % this.flapSpeed === 0){
             this.frame > 4 ? this.frame = 0 : this.frame++;
