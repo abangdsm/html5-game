af5574c0583d27466d39a232afa284051a273258
abangdsm <id.reversal@gmail.com>
2023-01-23 04:09:28 +0700

    Menambahkan karakter enemy ke 3 dan implementasi trigonometri

--- enemy-types/script.js
+++ enemy-types/script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById('cvs1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDHT = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numOfEnemies = 10;
+const numOfEnemies = 50;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -19,23 +19,25 @@ let gameFrame = 0;
 class Enemy {
     constructor() {
         this.image = new Image();
-        this.image.src = 'enemy2.png';
+        this.image.src = 'enemy3.png';
         this.speed = Math.random() * 4 + 1;
-        this.sprWidth = 266;
-        this.sprHeight = 188;
+        this.sprWidth = 218;
+        this.sprHeight = 177;
         this.width = this.sprWidth / 2.5;
         this.height = this.sprHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.angle = 0;
-        this.angleSpeed = Math.random() * 0.2;
-        this.curve = Math.random() * 10;
+        this.angle = Math.random() * 500;
+        this.angleSpeed = Math.random() * 0.5 + 0.5;
+        // this.curve = Math.random() * 200 + 50;
     }
     update(){
-        this.x -= this.speed; 
-        this.y += this.curve * Math.sin(this.angle);
+        this.x = canvas.width/2 * Math.cos(this.angle * Math.PI/200) + (canvas.width/2 - this.width/2);
+
+        this.y = canvas.height/2 * Math.sin(this.angle * Math.PI/300) + (canvas.height/2 - this.height/2);
+
         this.angle += this.angleSpeed;
         if (this.x + this.width < 0) this.x = canvas.width;
         // animate sprites
