e04233e0fdfa40ef911cf59f2ac3cebff0e4fed0
abangdsm <id.reversal@gmail.com>
2023-01-25 13:54:47 +0700

    Menambahkan animasi pada saat cursor bergerak

--- animation_on_trigger/script.js
+++ animation_on_trigger/script.js
@@ -13,12 +13,13 @@ class Explosion {
         this.spriteHeight = 179;
         this.width = this.spriteWidth * 0.7;
         this.height = this.spriteHeight * 0.7;
-        this.x = x - this.width/2;
-        this.y = y - this.height/2;
+        this.x = x;
+        this.y = y;
         this.image = new Image();
         this.image.src = 'boom.png';
         this.frame = 0;
         this.timer = 0;
+        this.angle = Math.random() * 6.2;
     }
     update(){
         this.timer++;
@@ -27,18 +28,26 @@ class Explosion {
         }
     }
     draw(){
-        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, 0 - this.width/2, 0 - this.height/2, this.width, this.height);
+        ctx.restore();
     }
 }
 
 window.addEventListener('click', function(e){
+    createAnimation(e);
+});
+
+window.addEventListener('mousemove', function(e){
+    createAnimation(e);
+});
+
+function createAnimation(e){
     let positionX = e.x - canvasPosition.left;
     let positionY = e.y - canvasPosition.top;
     explosions.push(new Explosion(positionX, positionY));
-});
-
-function createAnimation(){
-    
 }
 
 function animate(){
