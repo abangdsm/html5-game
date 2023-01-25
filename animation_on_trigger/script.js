b490a0858ea09ef321865a91d0804e7954b052db
abangdsm <id.reversal@gmail.com>
2023-01-25 14:03:47 +0700

    Menambahkan sound effect saat mouse diklik

--- animation_on_trigger/script.js
+++ animation_on_trigger/script.js
@@ -20,8 +20,11 @@ class Explosion {
         this.frame = 0;
         this.timer = 0;
         this.angle = Math.random() * 6.2;
+        this.sound = new Audio();
+        this.sound.src = 'boom.wav';
     }
     update(){
+        if(this.frame === 0) this.sound.play();
         this.timer++;
         if(this.timer % 10 === 0){
             this.frame++;
@@ -40,9 +43,9 @@ window.addEventListener('click', function(e){
     createAnimation(e);
 });
 
-window.addEventListener('mousemove', function(e){
+/*window.addEventListener('mousemove', function(e){
     createAnimation(e);
-});
+});*/
 
 function createAnimation(e){
     let positionX = e.x - canvasPosition.left;
