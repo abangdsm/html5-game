34c9ddaa01dcfdcf01dcad0fc57879cd8df7dd78
abangdsm <id.reversal@gmail.com>
2023-01-25 13:41:09 +0700

    Menambahkan event handler dengan animasi

--- animation_on_trigger/script.js
+++ animation_on_trigger/script.js
@@ -9,18 +9,22 @@ let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
     constructor(x, y){
-        this.x = x;
-        this.y = y;
         this.spriteWidth = 200;
         this.spriteHeight = 179;
-        this.width = this.spriteWidth/2;
-        this.height = this.spriteHeight/2;
+        this.width = this.spriteWidth * 0.7;
+        this.height = this.spriteHeight * 0.7;
+        this.x = x - this.width/2;
+        this.y = y - this.height/2;
         this.image = new Image();
         this.image.src = 'boom.png';
         this.frame = 0;
+        this.timer = 0;
     }
     update(){
-        this.frame++;
+        this.timer++;
+        if(this.timer % 10 === 0){
+            this.frame++;
+        }
     }
     draw(){
         ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
@@ -28,7 +32,25 @@ class Explosion {
 }
 
 window.addEventListener('click', function(e){
-    console.log(e);
-    ctx.fillStyle = 'white';
-    ctx.fillRect(e.x - canvasPosition.left - 15, e.y - canvasPosition.top - 15, 30, 30);
+    let positionX = e.x - canvasPosition.left;
+    let positionY = e.y - canvasPosition.top;
+    explosions.push(new Explosion(positionX, positionY));
 });
+
+function createAnimation(){
+    
+}
+
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for(let i = 0; i < explosions.length; i++){
+        explosions[i].update();
+        explosions[i].draw();
+        if(explosions[i].frame > 5){
+            explosions.splice(i, 1);
+            i--;
+        }
+    }
+    requestAnimationFrame(animate);
+};
+animate();
