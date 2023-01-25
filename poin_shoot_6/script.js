0d8c29c60633893d89b586730d59064b7bfcbf9a
abangdsm <id.reversal@gmail.com>
2023-01-25 15:53:10 +0700

    Mengubah player image, frame sprite width dan sprite height

--- poin_shoot_6/script.js
+++ poin_shoot_6/script.js
@@ -12,18 +12,30 @@ let lastTime = 0;
 let ravens = [];
 class Raven {
     constructor(){
-        this.width = 100;
-        this.height = 50;
+        this.spriteWidth = 271;
+        this.spriteHeight = 194;
+        this.sizeModifier = Math.random() * 0.6 + 0.4;
+        this.width = this.spriteWidth * this.sizeModifier;
+        this.height = this.spriteHeight * this.sizeModifier;
         this.x = canvas.width;
         this.y = Math.random() * (canvas.height - this.height);
         this.directionX = Math.random() * 5 + 3;
         this.directionY = Math.random() * 5 - 2.5;
+        this.markedForDeletion = false;
+        this.image = new Image();
+        this.image.src = 'raven.png';
+        this.frame = 0;
+        this.maxFrame = 4;
     }
     update(){
         this.x -= this.directionX;
+        if(this.x < 0 - this.width) this.markedForDeletion = true;
+        if(this.frame > this.maxFrame) this.frame = 0;
+        else this.frame++;
     }
     draw(){
-        ctx.fillRect(this.x, this.y, this.width ,this.height);
+        // ctx.strokeRect(this.x, this.y, this.width ,this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
@@ -41,6 +53,7 @@ function animate(timestamp){
     
     [...ravens].forEach(object => object.update());
     [...ravens].forEach(object => object.draw());
+    ravens = ravens.filter(object => !object.markedForDeletion);
 
     requestAnimationFrame(animate);
 }
