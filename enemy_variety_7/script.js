c10f674c1be739d183bad78aac53b6a040475235
abangdsm <id.reversal@gmail.com>
2023-01-26 12:48:25 +0700

    Minor bug

--- enemy_variety_7/script.js
+++ enemy_variety_7/script.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function(){
             }else{
                 this.enemyTimer++;
             }
-            this.enemies.forEach(object => object.update());
+            this.enemies.forEach(object => object.update(deltaTime));
         }
         draw(){
             this.enemies.forEach(object => object.draw(this.ctx));
@@ -40,26 +40,27 @@ document.addEventListener('DOMContentLoaded', function(){
             // console.log(this.game);
             this.markedForDeletion = false;
         }
-        update(){
-            this.x--;
+        update(deltaTime){
+            this.x -= this.vx * deltaTime;
             // remove enemies
             if(this.x < 0 - this.width) this.markedForDeletion = true;
         }
         draw(ctx){
-            // ctx.fillRect(this.x, this.y, this.width, this.height);
-            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
         }
     }
 
     class Worm extends Enemy {
         constructor(game){
             super(game);
+            this.spriteWidth = 229;
+            this.spriteHeight = 171;
+            this.width = this.spriteWidth/2;
+            this.height = this.spriteHeight/2;
             this.x = this.game.width;
             this.y = Math.random() * this.game.height;
-            this.width = 200;
-            this.height = 100;
             this.image = worm;
-            console.log(this.image);
+            this.vx = Math.random() * 0.1 + 0.1;
         }
     }
 
